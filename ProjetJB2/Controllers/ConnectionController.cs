22d9e41ce505277421bc7757795758535dacc973
Steven Grey <stevengrey45@gmail.com>
2018-06-19 13:12:57 +0200

    Back - Ajout du Controller de Connexion + Ajout du Formulaire d'Upload de Fichier dans la fenêtre détail des Jalons Front - Amélioration de quelques détails d'affichage (notamment les titres)

---
+++ ProjetJB2/Controllers/ConnectionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetJB2.Controllers
+{
+    public class ConnectionController : Controller
+    {
+        // GET: Connection
+        public ActionResult Index()
+        {
+			return View("Views/Connection/index.cshtml");
+        }
+    }
+}
