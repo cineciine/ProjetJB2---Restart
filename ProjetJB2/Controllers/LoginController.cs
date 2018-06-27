e024e1ec12d9cc19b2cc2b493c5b314c3f79691b
IBN SALAH Yacine <yis75012@hotmail.com>
2018-06-28 00:13:26 +0200

    page de connexion ok

---
+++ ProjetJB2/Controllers/LoginController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetJB2.Controllers
+{
+	public class LoginController : Controller
+	{
+		// GET: Login
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		/*Méthode de Connexion*/
+		public ActionResult signIn(string login, string password, string stat)
+		{
+			Console.WriteLine(login, password, stat);
+
+			string motDePasseEncode = "";//EncodeMD5(motDePasse);
+										 //return .Utilisateurs.FirstOrDefault(u => u.Prenom == nom && u.MotDePasse == motDePasseEncode);
+										 /*if(login != "" && password != "")	//Vérification que les variables ne soient pas vides
+										 {
+											 if (stat == "Student") {	//Contrôle du statut (Etudiant/Enseignant)
+
+											 }
+											 else {	//Statut == "Teacher"
+
+											 }
+
+											 foreach()	//Parcours des données
+											 {
+												 if() {	//Comparaison entre les données et les informations saisies
+												 }
+												 else {
+													 //Message d'Erreur
+												 }
+											 }
+
+										 }*/
+			return RedirectToAction("Index");
+		}
+
+		/*Méthode de Déconnexion*/
+		public ActionResult signOut()
+		{
+			return RedirectToAction("Index");
+		}
+	}
+}
